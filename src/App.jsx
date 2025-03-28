54a1e464900f424f9821e8a3ba0cffb0510d2c4d
Matheus Costa <contato.matheusccp5@gmail.com>
2025-03-28 12:03:13 -0300

    feat: add section gallery

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Header from './components/header'
 import Products from './components/products'
+import Gallery from './components/gallery'
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <>
       <Header />
       <Products />
+      <Gallery />
     </>
   )
 }
---
+++ src/components/gallery.jsx
@@ -0,0 +1,21 @@
+import img1 from '../assets/img-gallery1.png';
+import img2 from '../assets/img-gallery2.png';
+import img3 from '../assets/img-gallery3.png';
+
+export default function Gallery() {
+    return (
+        <div class="flex flex-wrap justify-center gap-4 p-8 px-6 md:pt-16 md:px-20 md:pb-16 md:h-[845px]">
+            <div className="flex flex-col w-full md:w-1/2 md:w-[500px] gap-6 md:gap-[34px]">
+                <div className="flex flex-col gap-[8px]">
+                    <h3 className='text-2xl md:text-[40px] font-bold'>Lorem ipsum dolor sit amet consectetur</h3>
+                    <p className='text-[16px] md:text-2xl font-thin'>Lorem ipsum dolor sit amet consectetur. Semper orci adipiscing faucibus sit scelerisque quis commodo</p>
+                </div>
+                <img src={img1} alt="" className=''/>
+            </div>
+            <div class="flex flex-col w-full md:w-1/2 gap-[8px]">
+                <img src={img2} alt="" />
+                <img src={img3} alt="" />
+            </div>
+        </div>
+    )
+}
